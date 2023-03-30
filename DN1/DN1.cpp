4935e720c94eddd64b2a45274da43108379e4645
TinKlancnik <tin.klancnik@student.um.si>
2023-03-30 15:28:03 +0200

    function for radixSort done

--- DN1/DN1.cpp
+++ DN1/DN1.cpp
@@ -5,31 +5,38 @@
 
 using namespace std;
 
-int main(int argc, char* argv[])
-{
-    cout << "File" << argv[1] << endl;
-    // Check if input file is provided
-    if (argc != 2) {
-        cout << "Usage: " << argv[0] << " <input_file>\n";
-        return 1;
-    }
+void readFromFile(const string& filename, vector<int>& numbers) {
+
 
     // Open input file
-    ifstream inputFile(argv[1]);
+    ifstream inputFile(filename);
     if (!inputFile) {
         cout << "Error: Could not open input file\n";
-        return 1;
+        return;
     }
 
-    vector<int> numbers;
     string line;
     while (getline(inputFile, line)) {
         istringstream iss(line);
         int num;
         while (iss >> num) {
             numbers.push_back(num);
-            cout << num << ", ";
         }
     }
-        return 0;
+}
+void radixSort(vector<int>& arr)
+{
+    int maxVal = 255;
+
+    for (int k = 0; maxVal >> k > 0; k++) {
+        //countingSort(arr, exp);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    vector<int> numbers;
+    readFromFile(argv[1], numbers);
+
+    return 0;
 }
