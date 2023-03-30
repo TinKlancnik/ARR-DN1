80d142522ee89664a512d1fb38f39552e0ca00da
TinKlancnik <tin.klancnik@student.um.si>
2023-03-30 12:00:09 +0200

    Storing numbers in to vector done

--- DN1/DN1.cpp
+++ DN1/DN1.cpp
@@ -1,43 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <vector>
 
 using namespace std;
-
-// Stable counting sort algorithm for sorting the bits
-void countingSort(vector<unsigned char>& arr, int exp)
-{
-    vector<unsigned char> output(arr.size());
-    vector<int> count(2, 0);
-
-    for (int i = 0; i < arr.size(); i++) {
-        count[(arr[i] >> exp) & 1]++;
-    }
-
-    for (int i = 1; i < count.size(); i++) {
-        count[i] += count[i - 1];
-    }
-
-    for (int i = arr.size() - 1; i >= 0; i--) {
-        output[count[(arr[i] >> exp) & 1] - 1] = arr[i];
-        count[(arr[i] >> exp) & 1]--;
-    }
-
-    for (int i = 0; i < arr.size(); i++) {
-        arr[i] = output[i];
-    }
-}
-
-// Binary Radix sort algorithm
-void radixSort(vector<unsigned char>& arr)
-{
-    int maxVal = 255; // maximum value of 8-bit unsigned integer
-
-    // Sort each bit using counting sort algorithm
-    for (int exp = 0; maxVal >> exp > 0; exp++) {
-        countingSort(arr, exp);
-    }
-}
 
 int main(int argc, char* argv[])
 {
@@ -55,27 +21,15 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    // Read input numbers into vector
-    vector<unsigned char> numbers;
-    unsigned char num;
-    while (inputFile >> num) {
-        numbers.push_back(num);
+    vector<int> numbers;
+    string line;
+    while (getline(inputFile, line)) {
+        istringstream iss(line);
+        int num;
+        while (iss >> num) {
+            numbers.push_back(num);
+            cout << num << ", ";
+        }
     }
-
-    // Sort the numbers using Binary Radix sort algorithm
-    radixSort(numbers);
-
-    // Open output file
-    ofstream outputFile("out.txt");
-    if (!outputFile) {
-        cout << "Error: Could not open output file\n";
-        return 1;
-    }
-
-    // Write sorted numbers to output file
-    for (const auto& num : numbers) {
-        outputFile << static_cast<int>(num) << " ";
-    }
-
-    return 0;
+        return 0;
 }
