3b16eca20870de05658d5bb188e0c7b787aa0960
TinKlancnik <tin.klancnik@student.um.si>
2023-03-31 09:22:04 +0200

    counting sort function done

--- DN1/DN1.cpp
+++ DN1/DN1.cpp
@@ -15,6 +15,30 @@ void readFromFile(const string& filename, vector<int>& numbers) {
     }
 }
 
+void countingSort(vector<int>& arr, int exp)
+{
+    vector<int> output(arr.size());
+    vector<int> count(2, 0);
+
+    for (int i = 0; i < arr.size(); i++) {
+        count[(arr[i] >> exp) & 1]++;
+    }
+
+    for (int i = 1; i < count.size(); i++) {
+        count[i] += count[i - 1];
+    }
+
+    for (int i = arr.size() - 1; i >= 0; i--) {
+        output[count[(arr[i] >> exp) & 1] - 1] = arr[i];
+        count[(arr[i] >> exp) & 1]--;
+    }
+
+    for (int i = 0; i < arr.size(); i++) {
+        arr[i] = output[i];
+    }
+}
+
+
 void radixSort(vector<int>& arr)
 {
     int maxVal = INT_MIN;
@@ -25,7 +49,7 @@ void radixSort(vector<int>& arr)
     }
 
     for (int k = 0; maxVal >> k > 0; k++) {
-       // countingSort(arr, k);
+       countingSort(arr, k);
     }
 }
 int main(int argc, char* argv[])
